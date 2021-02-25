0071a69c7e3fa05a5c6deb03ce0386528507ec9e
avneeshsaxena <saxena.avi009@gmail.com>
2021-02-25 23:35:40 +0530

    tilak

--- ReverseString.java
+++ ReverseString.java
@@ -4,7 +4,7 @@ public class ReverseString {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String str = "Sandeep";
+		String str = "Tilak";
 		StringBuilder rev=new StringBuilder("");
 		int leng = str.length();
 			
