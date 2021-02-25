80e4878341c786ef4a312a2e46b132bba6780de8
avneeshsaxena <saxena.avi009@gmail.com>
2021-02-25 23:33:36 +0530

    minor

--- ReverseString.java
+++ ReverseString.java
@@ -4,7 +4,7 @@ public class ReverseString {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String str = "Saxena";
+		String str = "Sandeep";
 		StringBuilder rev=new StringBuilder("");
 		int leng = str.length();
 			
